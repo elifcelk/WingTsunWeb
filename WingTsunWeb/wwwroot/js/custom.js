a81ca85e3a8c7858c554ecef8cc904e6d3223d62
elifclk444@gmail.com <elifclk444@gmail.com>
2023-01-22 22:19:05 +0300

    Add project files.

---
+++ WingTsunWeb/wwwroot/js/custom.js
@@ -0,0 +1,11 @@
+﻿
+(function ($) {
+
+    "use strict";
+
+    // NAVBAR
+    $('.navbar-nav .nav-link').click(function () {
+        $(".navbar-collapse").collapse('hide');
+    });
+
+})(window.jQuery);
